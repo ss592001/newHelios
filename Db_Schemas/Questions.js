f1087acaa2b67d65f46dcf44064ac4538f149595
ss592001 <90674743+ss592001@users.noreply.github.com>
2025-06-20 00:02:53 +0530

    Update Questions.js

--- Db_Schemas/Questions.js
+++ Db_Schemas/Questions.js
@@ -1,4 +1,51 @@
 
+// const { ObjectId } = require('mongodb');
+// const mongoose = require('mongoose');
+
+// const Schema = mongoose.Schema;
+// const Question = new Schema({
+//     id: {
+//         type: String
+//     },
+//     adminId: {
+//         type: ObjectId
+//     },
+//     title: {
+//         type: String
+//     },
+//     tags: {
+//         type: Array
+//     },
+//     passage: {
+//         type: String
+//     },
+//     question: {
+//         type: String
+//     },
+//     explanation: {
+//         type: String
+//     },
+//     answer: {
+//         type: String
+//     },
+
+//     options: {
+//         type: Array
+//     },
+//     difficulty: {
+//         type: String
+//     },
+//     type: {
+//         type: String
+//     },
+//     diagram: {
+//         type: String
+//     }
+// })
+// module.exports = mongoose.model('Question', Question);
+
+
+
 const { ObjectId } = require('mongodb');
 const mongoose = require('mongoose');
 
@@ -39,6 +86,12 @@ const Question = new Schema({
         type: String
     },
     diagram: {
+        type: String
+    },
+    topic: {
+        type: String
+    },
+    subTopic: {
         type: String
     }
 })
