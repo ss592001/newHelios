56a63486ec250babe328e62055a4597a092f1a0a
ss592001 <90674743+ss592001@users.noreply.github.com>
2026-07-14 04:30:25 +0530

    Update SSL certificate options in index.js

--- index.js
+++ index.js
@@ -14,16 +14,16 @@ const mongoose = require('mongoose');
 const AuthRoutes = require('./Routes/Auth/Auth');
 const AdminRoutes=require('./Routes/Admin/Admin')
 // Rvgo
-// const options = {
-//   key: fs.readFileSync("/etc/letsencrypt/live/srv749425.hstgr.cloud/privkey.pem"),
-//   cert: fs.readFileSync("/etc/letsencrypt/live/srv749425.hstgr.cloud/fullchain.pem"),
-// };
+const options = {
+  key: fs.readFileSync("/etc/letsencrypt/live/srv749425.hstgr.cloud/privkey.pem"),
+  cert: fs.readFileSync("/etc/letsencrypt/live/srv749425.hstgr.cloud/fullchain.pem"),
+};
 
 // Buzz
-const options = {
-  key: fs.readFileSync("/etc/letsencrypt/live/srv1295924.hstgr.cloud/privkey.pem"),
-  cert: fs.readFileSync("/etc/letsencrypt/live/srv1295924.hstgr.cloud/fullchain.pem"),
-};
+// const options = {
+//   key: fs.readFileSync("/etc/letsencrypt/live/srv1295924.hstgr.cloud/privkey.pem"),
+//   cert: fs.readFileSync("/etc/letsencrypt/live/srv1295924.hstgr.cloud/fullchain.pem"),
+// };
 const mongoUrl = process.env.DbUrl; 
 const Store = new MongoDbStore({
    uri: mongoUrl,
